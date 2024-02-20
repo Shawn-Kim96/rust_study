151323ee1f5baed910650fe21c730675c6888769
suhyun.kim <suhyun.kim@42dot.ai>
2024-02-20 14:08:22 +0900

    study result and chapter 4

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -5,6 +5,7 @@ struct User {
     active: bool,
 }
 
+<<<<<<< HEAD
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
@@ -24,3 +25,18 @@ fn main() {
     let black = Color(0,0,0);
     let origin = Point(0,0,0);
 }
+=======
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("another@email.com");
+    println!("{user1}");
+}
+
+
+>>>>>>> 7706a7a (study result and chapter 4)
