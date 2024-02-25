7842e2803776e0f9a90717fbb5e742dec2e86ca5
Shawn-Kim96 <dun03191@gmail.com>
2024-02-25 13:37:42 +0900

    Chapter 9 end

---
+++ ch9/error3/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
